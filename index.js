13d6456cfbdc82be689a07181b4ff7c5f7381a33
NSSFM <u.sem@icloud.com.com>
2023-02-16 19:26:37 +0100

    trying to add a dynamic checkboxes

--- index.js
+++ index.js
@@ -6,11 +6,32 @@ class City {
     }
 }
 
-const cities = [
+const CITIES = [
     new City("Warsaw", 100, ["Cool"]),
     new City("Lublin", 2),
     new City("Gdansk", 10, ["Cool", "Interesting", "Sexy"])
 ];
+
+const KEYWORDS = [
+    "Cool", "Interesting", "Sexy"
+]
+
+const checkboxContainer = document.getElementById("dynamicBoxes");
+
+for (let addBox = 0; addBox < KEYWORDS.length; addBox++) {
+    const key = KEYWORDS[addBox];
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.value = key;
+    checkbox.id = key;
+
+    const label = document.createElement("label");
+    label.htmlFor = key;
+    label.innerText = key;
+
+    checkboxContainer.appendChild(checkbox);
+    checkboxContainer.appendChild(label);
+}
 
 function filterByPopulation(citiesArr, from, to) {
     const result = citiesArr.filter(city => city.population >= from && city.population <= to);
@@ -19,15 +40,16 @@ function filterByPopulation(citiesArr, from, to) {
 function filterByKeywords(citiesArr, arrKeys = []) {
     let isAnyChecked = false;
 
-    for(let key of arrKeys) {
-        if(key.checked) {
+    for (let key of arrKeys) {
+        if (key.checked) {
             isAnyChecked = true;
             break;
         }
     }
+
     let result;
-    if(isAnyChecked)
-        result = citiesArr.filter(c => c.keywords.some(k => arrKeys[k]?.checked));
+    if (isAnyChecked)
+        result = citiesArr.filter(c => c.keywords.some(k => arrKeys.includes(k)));
     else
         result = citiesArr;
 
@@ -38,13 +60,15 @@ function filterByKeywords(citiesArr, arrKeys = []) {
 export function buttonClick() {
     const fromEl = document.getElementById("from");
     const toEl = document.getElementById("to");
-    let sortedCities = filterByPopulation(cities, fromEl.value, toEl.value);
+    let sortedCities = filterByPopulation(CITIES, fromEl.value, toEl.value);
 
     const resultDiv = document.getElementById("result");
-    const checkForm = document.getElementById("keyForms");
-    sortedCities = filterByKeywords(sortedCities, checkForm.elements);
+    const checkForm = document.getElementById("dynamicBoxes");
+    sortedCities = filterByKeywords(sortedCities, checkForm.elements.map(el => el));
 
     const sresultDiv = document.getElementById("Sresult");
+
+
     resultDiv.innerHTML = "";
     sresultDiv.innerHTML = "";
     for (let city of sortedCities) {
@@ -54,7 +78,7 @@ export function buttonClick() {
 
         resultDiv.appendChild(cityDiv);
     };
+}
 
-};
-document.getElementById("buttonFilter").addEventListener("click", buttonClick);
+document.getElementById('buttonFilter').addEventListener('click', buttonClick);
 
